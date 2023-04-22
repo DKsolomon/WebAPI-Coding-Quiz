e81bc1d0c17d7a813859357124f6bd6290833a3c
DKsolomon <DKSolomon8135@gmail.com>
2023-04-22 15:57:05 -0700

    Updated array so that questions cycle through when answered

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 var quiz = document.getElementById("quiz");
 var timer = document.getElementById("timer");
-var score = document.getElementById("score");
-var question = document.getElementById("questions");
+var questionBox = document.getElementById("questions");
 var multipleChoice = document.getElementById("choices");  
 var checkAnswer = document.getElementById("check-answer");
 var startBtn = document.getElementById("start");
@@ -10,28 +9,28 @@ var welcomeText = document.getElementById("starter-page");
 var quizQuestions = [
     {
         question: "Inside which HTML element do we put the JavaScript?",
-        choices: ["<js>, <javascript>, <script>, <scripting> "],
+        choices: ["<js>", "<javascript>", "<script>", "<scripting>"],
         answer: "<script>",
 },   
     {
         question: "Which of the following keywords is used to define a variable in Javascript?",
-        choices: ["var, let, Option 1 and 2, None of the above"],
+        choices: ["var", "let", "Option 1 and 2", "None of the above"],
         answer: "Option 1 and 2",
    
 }, 
     {    
         question:"Which of the following methods is used to access HTML elements using Javascript?",
-        choices: ["getElementbyId(), getElementsByClassName(), Option 1 and 2, None of the above."],
+        choices: ["getElementbyId()", "getElementsByClassName()", "Option 1 and 2", "None of the above."],
         answer: "Option 1 and 2",
 },   
     {        
         question: "how can a datatype be declared to be a constant type?",
-        choices: ["const, var, let, constant"],
+        choices: ["const", "var", "let", "constant"],
         answer:"const",
 },   
     {
         question:"Which of the following methods can be used to display data in some form using Javascript?",
-        choices: ["document.write(), console.log(), window.alert, All of the above"],
+        choices: ["document.write()", "console.log()", "window.alert", "All of the above"],
         answer: "All of the above",
     }];
 
@@ -40,7 +39,6 @@ var quizQuestions = [
     var timeLeft = 60;
     var timerInterval = 0;
     var penalty = 10;
-    var score = 0; 
 
     function startQuiz() {
         welcomeText.setAttribute("class", "hide");
@@ -55,7 +53,7 @@ var quizQuestions = [
         timerInterval = setInterval(()  => {
             timeLeft--;
             timer.innerHTML =` ${timeLeft} seconds remaining`;
-            if(timeLeft === 0 || currentQuestionIndex === question.length) {
+            if(timeLeft === 0 || currentQuestionIndex === quizQuestions.length) {
                 clearInterval(timerInterval);
             } 
         
@@ -65,7 +63,7 @@ var quizQuestions = [
 
     function grabQuestions() {
         var currentQuestion = quizQuestions[currentQuestionIndex];
-        question.textContent = currentQuestion.question
+        questionBox.textContent = currentQuestion.question
 
         multipleChoice.innerHTML = "";
        
@@ -76,27 +74,25 @@ var quizQuestions = [
     
             selectChoice.textContent = i + 1 + ". " + choice;
         
-            selectChoice.addEventListener('click', checkAnswer);
+            selectChoice.addEventListener('click', answerCheck);
             multipleChoice.appendChild(selectChoice);
         }); 
     }
 
     function answerCheck() {
-        if (this.value !== question[currentQuestionIndex].answer) {
-            timer -= 15;
-
-            if (timer < 0) {
-                timer = 0;
-            }
-        timer.textContext = time;
+        if (this.value !== quizQuestions[currentQuestionIndex].answer) {
+            timer -= 10;
+            
         checkAnswer.textContext = "Wrong!";
+        checkAnswer.style.color = "red";
     } else {
         checkAnswer.textContext = "Correct!";
+        checkAnswer.style.color = "green" 
     }
 
-    currentQuestionIndex++;
+currentQuestionIndex++;
 
-    if (currentQuestionIndex === question.length) {
+    if (currentQuestionIndex === quizQuestions.length) {
         showScore();
     } else {
         grabQuestions();
